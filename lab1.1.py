1a852ef033c812a01c08760c1c25614b8f7155b2
Ruslan Ded <konozrusik@gmail.com>
2024-10-16 14:51:39 +0100

    Lab1.1 is done

---
+++ lab1.1.py
@@ -0,0 +1,21 @@
+def validation(num):
+	if num<=0:
+		print("Should be positive numbers")
+		exit(1)
+
+def main():
+	print("For this task you should enter natural numbers (more than 0)")
+	a = int(input("Enter the number for a:\n"))
+	validation(a)
+	b = int(input("Enter the number for b:\n"))
+	validation(b)
+	if a<b:
+		x=a/b +5
+	elif a==b:
+		x=-5
+	else:
+		x=(a*a-b)/b
+	print(f"Your x is {x}")
+
+if __name__ == '__main__':
+	main()
