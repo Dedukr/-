ce85f42967cdca551376649f676d1e79f6fa25e9
Ruslan Ded <konozrusik@gmail.com>
2024-10-16 16:03:15 +0100

    Lab1.3 is done

---
+++ lab1.3.py
@@ -0,0 +1,20 @@
+def validation(n):
+	if 1 < n < 9:
+		return
+	print("Invalid input")
+	exit(1)
+
+
+def main():
+	n = int(input("enter a number (1<N<9):\n"))
+	validation(n)
+
+	for i in range(1, n + 1):
+		for j in range(1, i + 1):
+			print(j, end="")
+		print(end="\n")
+
+
+
+if __name__ == '__main__':
+	main()
